cbafbde01ffc35797ef43f95bf13b2847ac75cfe
MateusRaposo <mateus.raposo6@gmail.com>
2026-07-23 21:39:14 -0300

    Update ex1.py
    Problem statement added

--- ex1.py
+++ ex1.py
@@ -1,3 +1,18 @@
+# Given two equal length strings representing two players' strategies for a game, return the scores as an array [player1, player2].
+
+# The given strings will only contain one of two letters: "C" (cooperate) or "D" (defect).
+# Each character represents one round, scored as follows:
+# If both players cooperate, each scores 3.
+# If both players defect, each scores 1.
+# If one player defects and the other cooperates, the defector scores 5 and the cooperator scores 0.
+# Tests:
+# Waiting:1. play_game("CCCC", "CCCC") should return [12, 12].
+# Waiting:2. play_game("DDDD", "DDDD") should return [4, 4].
+# Waiting:3. play_game("CCDD", "CDDD") should return [5, 10].
+# Waiting:4. play_game("CCCDCDCCCDDC", "CCDDCDCDDCCD") should return [24, 34].
+# Waiting:5. play_game("DDCCDDDDCDDCDDDCDD", "CCDCCCDCCCDCCCCDCC") should return [66, 21].
+
+
 def play(p1, p2):
     score_p1 = 0
     score_p2 = 0
